63290f663d1e62efc21755ab9268ffba4da6411b
rujinlong <jinlongru@gmail.com>
2023-07-02 12:04:32 +0200

    add R

---
+++ docker/devapar/create_dockerfile.R
@@ -0,0 +1,20 @@
+dir_build <- tempfile(pattern = "renv")
+dir.create(dir_build)
+
+# Create a lockfile
+the_lockfile <- file.path(dir_build, "renv.lock")
+custom_packages <- c(
+  # attachment::att_from_description(),
+  "tximeta"
+)
+renv::snapshot(
+  packages = custom_packages,
+  lockfile = the_lockfile,
+  prompt = FALSE
+)
+
+
+my_dock <- dockerfiler::dock_from_renv(lockfile = the_lockfile)
+my_dock$MAINTAINER("Jinlong Ru", "jinlong.ru@gmail.com")
+
+my_dock$write("Dockerfile")
