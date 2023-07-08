2614afb5f9ad91de7bf83221d321ed870cb75b17
rujinlong <jinlongru@gmail.com>
2023-07-08 11:00:13 +0200

    ignore tx2gene

--- bin/create_tximeta.r
+++ bin/create_tximeta.r
@@ -8,13 +8,12 @@ library(tximport)
 
 p <- arg_parser("Create tximeta object from salmon output")
 p <- add_argument(p, "--salmon", help = "salmon quant output directory (ex. cow_salmon)")
-p <- add_argument(p, "--ref2symbol", help = "ref2symbol.txt")
 p <- add_argument(p, "--fout", help = "output file name")
 argv <- parse_args(p)
 
-tx2gene <- fread(argv$ref2symbol) %>%
-    setnames(colnames(.), c("TXNAME", "GENEID")) %>%
-    dplyr::mutate(TXNAME = str_replace(TXNAME, "\\.[0-9]+$", ""))
+#tx2gene <- fread(argv$ref2symbol) %>%
+    #setnames(colnames(.), c("TXNAME", "GENEID")) %>%
+    #dplyr::mutate(TXNAME = str_replace(TXNAME, "\\.[0-9]+$", ""))
 
 
 dpath <- argv$salmon
@@ -23,6 +22,7 @@ spath <- file.path(dpath, sids)
 sfs <- file.path(spath, "quant.sf")
 names(sfs) <- str_replace(sids, "_.*", "")
 
-txi <- tximport(sfs, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+#txi <- tximport(sfs, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+txi <- tximport(sfs, type = "salmon", txOut = TRUE, ignoreTxVersion = TRUE)
 
 saveRDS(txi, file = argv$fout, compress = "xz")
