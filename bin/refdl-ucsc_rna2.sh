8950ce8da1f58a5c3a4cfbd4338560eaeb221b42
rujinlong <jinlongru@gmail.com>
2023-07-15 11:15:11 +0200

    fix

--- bin/refdl-ucsc_rna2.sh
+++ bin/refdl-ucsc_rna2.sh
@@ -5,12 +5,12 @@ wget -c -O RatNor.fa https://hgdownload.soe.ucsc.edu/gbdb/rn7/ncbiRefSeq/seqNcbi
 wget -c -O XenLae.fa https://hgdownload.soe.ucsc.edu/gbdb/xenLae2/ncbiRefSeq/seqNcbiRefSeq.rna.fa
 wget -c -O XenTro.fa https://hgdownload.soe.ucsc.edu/gbdb/xenTro9/ncbiRefSeq/seqNcbiRefSeq.rna.fa
 wget -c -O bosTau.fa https://hgdownload.soe.ucsc.edu/gbdb/bosTau9/ncbiRefSeq/seqNcbiRefSeq.rna.fa
-wget -c -O bosTau.fa https://hgdownload.soe.ucsc.edu/goldenPath/bosTau9/bigZips/refMrna.fa.gz
+wget -c -O bosTau2.fa https://hgdownload.soe.ucsc.edu/goldenPath/bosTau9/bigZips/refMrna.fa.gz
 wget -c -O susScr.fa https://hgdownload.soe.ucsc.edu/gbdb/susScr11/ncbiRefSeq/seqNcbiRefSeq.rna.fa
-wget -c -O susScr.fa https://hgdownload.soe.ucsc.edu/goldenPath/susScr11/bigZips/refMrna.fa.gz
+wget -c -O susScr2.fa https://hgdownload.soe.ucsc.edu/goldenPath/susScr11/bigZips/refMrna.fa.gz
 wget -c -O DroMel.fa https://hgdownload.soe.ucsc.edu/gbdb/dm6/ncbiRefSeq/seqNcbiRefSeq.rna.fa
 wget -c -O GalGal.fa https://hgdownload.soe.ucsc.edu/gbdb/galGal6/ncbiRefSeq/seqNcbiRefSeq.rna.fa
 wget -c -O CaeEle.fa https://hgdownload.soe.ucsc.edu/gbdb/ce11/ncbiRefSeq/seqNcbiRefSeq.rna.fa
 wget -c -O HomSap.fa https://hgdownload.soe.ucsc.edu/gbdb/hg38/ncbiRefSeq/seqNcbiRefSeq.rna.fa
-wget -c -O HomSap.fa https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/refMrna.fa.gz
+wget -c -O HomSap2.fa https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/refMrna.fa.gz
 wget -c -O DanRer.fa https://hgdownload.soe.ucsc.edu/gbdb/danRer11/ncbiRefSeq/seqNcbiRefSeq.rna.fa
